e76c5af8586db08f43f8a19ba2f6de285f7d8203
CodeingBoy <CodeingBoy@gmail.com>
2018-10-03 00:42:10 +0800

    Fixed bugs for favourite comments API

--- server/controllers/favourite.js
+++ server/controllers/favourite.js
@@ -3,7 +3,7 @@ const db = require('../utils/db.js');
 module.exports = {
   list: async ctx => {
     const openId = ctx.state.$wxInfo.userinfo.openId;
-    var comments = await db.query("SELECT c.* FROM favourite_comment AS f JOIN comment_info AS c WHERE s.open_id = ?", [movieId]);
+    var comments = await db.query("SELECT c.*, m.id AS movie_id, m.title, m.image FROM favourite_comment AS f JOIN comment_info AS c ON f.comment_id = c.id JOIN movies AS m ON c.movie_id = m.id WHERE f.open_id = ?", [openId]);
 
     ctx.state.data = comments.map(function(c) {
       var content = JSON.parse(c.content);
@@ -13,7 +13,10 @@ module.exports = {
         type: content.type,
         content,
         id: c.id,
-        nickname: c.nickname
+        nickname: c.nickname,
+        title: c.title,
+        image: c.image,
+        movieId: c.movie_id
       };
     })
   },
